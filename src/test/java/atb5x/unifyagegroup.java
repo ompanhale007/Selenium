e09607aeb083ac91686ac1b47654e67a66d909e0
ompanhale007 <ompanhale007@example.com>
2023-11-20 23:15:01 +0530

    My third Commit i have change the name of the file/class name from wait3 to unifyagegroup of Etp project

--- src/test/java/atb5x/unifyagegroup.java
+++ src/test/java/atb5x/unifyagegroup.java
@@ -7,13 +7,12 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class wait3 {
+public class unifyagegroup {
 
         WebDriver driver;
         WebDriverWait wait;
